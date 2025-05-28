b13b0eaabd10c33d7a72ea3d61785f951932df33
lud <ludovic@demblans.com>
2025-05-29 01:57:04 +0200

    chore: Configure libcheck

--- .libcheck.exs
+++ .libcheck.exs
@@ -1,3 +1,14 @@
 [
-  github_dependencies: true
+  github_dependencies: true,
+  dependencies: [
+    allow_prod_env: [
+      :credo,
+      :dialyxir,
+      :doctor,
+      :ex_check,
+      :ex_doc,
+      :mix_audit,
+      :sobelow
+    ]
+  ]
 ]
